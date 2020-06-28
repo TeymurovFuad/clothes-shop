2278fc01f315242736c6612a86cd5fbf36b58082
Fuad Teymurov <fuad.teymurov@github.com>
2020-06-29 02:22:37 +0400

    homepage divided into small components, styling files distributed by seperate folders and place into one style folder

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import "../../styles/css/directory.styles.css";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+          linkUrl: "shop/hats",
+          col: 4,
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+          linkUrl: "shop/jackets",
+          col: 4,
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+          linkUrl: "shop/sneakers",
+          col: 4,
+        },
+        {
+          title: "women",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens",
+          col: 6,
+        },
+        {
+          title: "men",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens",
+          col: 6,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        <div className="row4">
+          {this.state.sections.map(({ title, imageUrl, id }) => (
+            <MenuItem
+              key={id}
+              title={title.toUpperCase()}
+              imageUrl={imageUrl}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Directory;
---
+++ src/page/home/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../styles/css/homepage.styles.css";
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+  <div className="homePage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
