ffb38a35981742f65d072add4df8c8d149443759
Fuad Teymurov <fuad.teymurov@github.com>
2020-07-22 02:59:07 +0400

    card counter 'totalItems' was added which will count total number of item added to the card

--- src/components/card/card-icon.component.jsx
+++ src/components/card/card-icon.component.jsx
@@ -4,10 +4,11 @@ import { ReactComponent as ShoppingBagIcon } from "../../assests/shopping-bag.sv
 import { connect } from "react-redux";
 import { toggleCardIcon } from "../../redux/card/card.actions";
 
-const CardICon = ({ toggleCardIcon }) => (
+const CardICon = ({ toggleCardIcon, totalItems }) => (
   <div className="card-icon" onClick={toggleCardIcon}>
     <ShoppingBagIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    {/* style="background-color:"{`${totalItems< 10? "ff"+totalItems+"fff":"ff"+totalItems+"f"+totalItems%10+"ff"}`} */}
+    <span className="item-count">{totalItems}</span>
   </div>
 );
 
@@ -17,4 +18,11 @@ const mapDispatchProps = (dispatch) => ({
   toggleCardIcon: () => dispatch(toggleCardIcon()),
 });
 
-export default connect(null, mapDispatchProps)(CardICon);
+const mapStateToProps = ({ card: { cardItems } }) => ({
+  totalItems: cardItems.reduce(
+    (accumulatedQuantity, cardItem) => accumulatedQuantity + cardItem.quantity,
+    0
+  ),
+});
+
+export default connect(mapStateToProps, mapDispatchProps)(CardICon);
